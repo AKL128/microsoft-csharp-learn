6a4697938ed9d79342f5f28860d56667b77a5f7d
akl128 <anthonyluu128@gmail.com>
2023-07-26 15:36:10 -0500

    Added periodLocator

--- whileLoopFolder/periodLocator/Program.cs
+++ whileLoopFolder/periodLocator/Program.cs
@@ -1,2 +1,27 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿string[] myStrings = new string[2] {"I like pizza. I like roast chicken. I like salad", "I like all three of the menu choices"};
+
+int periodLocation = 0;
+
+string result = "";
+
+for (int i = 0; i < myStrings.Length; i++) 
+{
+    result = myStrings[i];
+
+    periodLocation = result.IndexOf(".");
+
+    string sentence;
+
+    while (periodLocation != -1)
+    {
+        sentence = result.Remove(periodLocation);
+        result = result.Substring(periodLocation+1);
+        result = result.TrimStart();
+        periodLocation = result.IndexOf(".");
+
+        Console.WriteLine(sentence);
+    }
+    
+    sentence = result.Trim();
+    Console.WriteLine(sentence);
+}
